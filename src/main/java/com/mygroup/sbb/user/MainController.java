75369170fe6d5ec6bee54c3d490f8bca3a1c5e73
SeongMun-Hwang <hwang105814@gmail.com>
2023-07-28 17:02:34 +0900

    root direction

--- src/main/java/com/mygroup/sbb/user/MainController.java
+++ src/main/java/com/mygroup/sbb/user/MainController.java
@@ -10,6 +10,11 @@ public class MainController {
     @GetMapping("/sbb")
     @ResponseBody
     public String index() {
-        return "<h1>안녕하세요. SBB에 오신 것을 환영합니다.</h1>";
+        return "안녕하세요 sbb에 오신것을 환영합니다.";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
     }
 }
