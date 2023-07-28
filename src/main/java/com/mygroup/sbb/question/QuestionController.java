02f50bb6627017acf9995d1de3faa46eb3d7b784
SeongMun-Hwang <hwang105814@gmail.com>
2023-07-28 17:21:26 +0900

    service for data process

--- src/main/java/com/mygroup/sbb/question/QuestionController.java
+++ src/main/java/com/mygroup/sbb/question/QuestionController.java
@@ -1,22 +1,22 @@
 package com.mygroup.sbb.question;
 
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 @Controller
 public class QuestionController {
+    private final QuestionService questionService;
 
-    private final QuestionRepository questionRepository;
+    // Inject QuestionService using constructor
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
 
     @GetMapping("/question/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
