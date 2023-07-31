92be2be47184534ae7fbf799404d51fef8564ab1
hwang105814 <83663987+SeongMun-Hwang@users.noreply.github.com>
2023-07-31 11:57:04 +0900

    Revert "apply paging"

--- src/test/java/com/mygroup/sbb/SbbApplicationTests.java
+++ src/test/java/com/mygroup/sbb/SbbApplicationTests.java
@@ -1,23 +1,33 @@
 package com.mygroup.sbb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.mygroup.sbb.question.QuestionService;
+import com.mygroup.sbb.answer.Answer;
+import com.mygroup.sbb.answer.AnswerRepository;
+import com.mygroup.sbb.question.QuestionRepository;
 
 @SpringBootTest
 class SbbApplicationTests {
 
-    @Autowired
-    private QuestionService questionService;
+	@Autowired
+	private QuestionRepository questionRepository;
 
-    @Test
-    void testJpa() {
-        for (int i = 1; i <= 300; i++) {
-            String subject = String.format("테스트 데이터입니다:[%03d]", i);
-            String content = "내용무";
-            this.questionService.create(subject, content);
-        }
-    }
+	@Autowired
+	private AnswerRepository answerRepository;
+
+	@Test
+	void testJpa() {
+		Optional<Answer> oa = this.answerRepository.findById(1);
+		assertTrue(oa.isPresent());
+		Answer a = oa.get();
+		assertEquals(2, a.getQuestion().getId());
+	}
 }
