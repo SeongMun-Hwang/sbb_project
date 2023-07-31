ae896ed31566f8ca19cda158de508a51a715cee9
SeongMun-Hwang <hwang105814@gmail.com>
2023-07-31 16:06:10 +0900

    add change question

--- src/main/java/com/mygroup/sbb/question/QuestionService.java
+++ src/main/java/com/mygroup/sbb/question/QuestionService.java
@@ -33,16 +33,23 @@ public class QuestionService {
     public Page<Question> getList(int page) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
-        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        Pageable pageable = PageRequest.of(page, 10,Sort.by(sorts));
         return this.questionRepository.findAll(pageable);
     }
 
-    public Question getQuestion(Integer id) {
+    public Question getQuestion(Integer id) {  
         Optional<Question> question = this.questionRepository.findById(id);
         if (question.isPresent()) {
             return question.get();
         } else {
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void modify(Question question, String subject, String content) {
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setModifyDate(LocalDateTime.now());
+        this.questionRepository.save(question);
     }
 }
