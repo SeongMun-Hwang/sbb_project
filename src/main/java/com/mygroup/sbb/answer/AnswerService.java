c0acb6bfe84cfc5b9e72e56be507910310adaf07
SeongMun-Hwang <hwang105814@gmail.com>
2023-07-31 16:11:53 +0900

    add answer change

--- src/main/java/com/mygroup/sbb/answer/AnswerService.java
+++ src/main/java/com/mygroup/sbb/answer/AnswerService.java
@@ -8,11 +8,15 @@ import com.mygroup.sbb.user.SiteUser;
 
 import java.time.LocalDateTime;
 
+import java.util.Optional;
+import com.mygroup.sbb.DataNotFoundException;
+
 @RequiredArgsConstructor
 @Service
 public class AnswerService {
 
     private final AnswerRepository answerRepository;
+
 
     public void create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
@@ -20,6 +24,21 @@ public class AnswerService {
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
         answer.setAuthor(author);
+        this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
         this.answerRepository.save(answer);
     }
 }
