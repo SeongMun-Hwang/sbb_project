e400087064e24af1eb4e9da3c5d90612d71704dc
SeongMun-Hwang <hwang105814@gmail.com>
2023-08-01 14:22:49 +0900

    divide local/server working environment

--- src/test/java/com/mygroup/sbb/SbbApplicationTests.java
+++ src/test/java/com/mygroup/sbb/SbbApplicationTests.java
@@ -1,33 +1,33 @@
-package com.mygroup.sbb;
+// package com.mygroup.sbb;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
+// import java.time.LocalDateTime;
+// import java.util.Optional;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-import com.mygroup.sbb.answer.Answer;
-import com.mygroup.sbb.answer.AnswerRepository;
-import com.mygroup.sbb.question.QuestionRepository;
+// import com.mygroup.sbb.answer.Answer;
+// import com.mygroup.sbb.answer.AnswerRepository;
+// import com.mygroup.sbb.question.QuestionRepository;
 
-@SpringBootTest
-class SbbApplicationTests {
+// @SpringBootTest
+// class SbbApplicationTests {
 
-	@Autowired
-	private QuestionRepository questionRepository;
+// @Autowired
+// private QuestionRepository questionRepository;
 
-	@Autowired
-	private AnswerRepository answerRepository;
+// @Autowired
+// private AnswerRepository answerRepository;
 
-	@Test
-	void testJpa() {
-		Optional<Answer> oa = this.answerRepository.findById(1);
-		assertTrue(oa.isPresent());
-		Answer a = oa.get();
-		assertEquals(2, a.getQuestion().getId());
-	}
-}
+// @Test
+// void testJpa() {
+// Optional<Answer> oa = this.answerRepository.findById(1);
+// assertTrue(oa.isPresent());
+// Answer a = oa.get();
+// assertEquals(2, a.getQuestion().getId());
+// }
+// }
